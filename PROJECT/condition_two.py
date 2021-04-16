663e31595c22628f91c1a60a3c81e8ab62d8548d
Jalilov <jalilovjavohir777@gmail.com>
2021-04-16 11:08:49 +0500

    write code to the function

--- PROJECT/condition_two.py
+++ PROJECT/condition_two.py
@@ -8,5 +8,15 @@ def plot_data(gender:list,weight:list,height:list):
     h_male = []
     h_female = []
     #WRITE YOUR CODE HERE
-    
+    for i in range(len(gender)):
+        if gender[i] == 1:
+            w_male.append(weight[i])
+            h_male.append(height[i])
+        else:
+            w_female.append(weight[i])
+            h_female.append(height[i])
+    plt.scatter(w_male,h_male,c='k',label='Male',alpha=0.6)
+    plt.scatter(w_female,h_female,c='r',label='Female',alpha=0.6)
+    plt.legend(loc='upper left',ncol=2,bbox_to_anchor=(0,1.125))
+
     plt.show()
