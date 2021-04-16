ed7c1e26b64db014583f8721b34467837a801a82
Jalilov <jalilovjavohir777@gmail.com>
2021-04-16 11:03:43 +0500

    create function

--- PROJECT/condition_one.py
+++ PROJECT/condition_one.py
@@ -1,3 +1,10 @@
 import matplotlib.pyplot as plt
 
 data = open('weight-height.csv')
+
+def get_data(data):
+    gender = []
+    weight = []
+    height = []
+        
+    return gender,weight,height
