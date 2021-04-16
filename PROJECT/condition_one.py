1c1a9c5a7c9ae852a281bc5fe73b30d34619db26
Jalilov <jalilovjavohir777@gmail.com>
2021-04-16 11:05:26 +0500

    finally condition one

--- PROJECT/condition_one.py
+++ PROJECT/condition_one.py
@@ -1,10 +1,15 @@
-import matplotlib.pyplot as plt
-
-data = open('weight-height.csv')
-
 def get_data(data):
     gender = []
     weight = []
     height = []
+    # WRITE YOUR CODE HERE
+    data_list = data.read().split('\n')
+    g,h,w = data_list.pop(0).split(',')
+
+    for item in data_list:
+        gen,heig,weig = item.split(',')
+        gender.append(1) if gen == '"Male"' else gender.append(0)
+        weight.append(float(weig)/ 2.205)
+        height.append(float(heig)*2.54)
         
     return gender,weight,height
