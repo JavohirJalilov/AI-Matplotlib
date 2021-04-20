c33db06738a19d850dbb57731fb6191e05ffebc3
Jalilov <jalilovjavohir777@gmail.com>
2021-04-20 23:18:43 +0500

    csv reader rows seperater

---
+++ MNIST_dataset/main.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+import csv
+
+data = open('mnist_test.csv')
+def get_data(data):
+    reader = csv.reader(data)
+    dataset = []
+    for rows in reader:
+        k = 1
+        number_data = []
+        row = []
+        for i in rows:
+            row.append(int(i))
+            if k == 28:
+                number_data.append(row)
+                k = 0
+                row = []
+            k += 1
+        dataset.append(number_data)
+
+    return dataset
